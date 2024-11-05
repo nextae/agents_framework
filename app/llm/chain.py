2fea26dff851e05a057b92a096b85fa0f99ccfcc
Karol Wójciak <karix.wojciak@gmail.com>
2024-11-05 15:32:14 +0100

    #12 - Use structured output instead of tools

--- app/llm/chain.py
+++ app/llm/chain.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable
 from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field, create_model
 
 from app.llm.models import Action
 
@@ -12,8 +13,32 @@ load_dotenv()
 OPENAI_MODEL = os.getenv("OPENAI_MODEL")
 
 
-# TODO: make a model for the input data
-def create_chain(system_message: str, actions: list[Action]) -> Runnable:
+# TODO: make this a method of the Agent class e.g. agent.to_structured_output()
+def create_structured_output(actions: list[Action]) -> type[BaseModel]:
+    """Creates a Pydantic model for the structured output."""
+
+    actions_model = create_model(
+        "Actions",
+        **{
+            action.name: (
+                action.to_structured_output() | None,
+                Field(..., description=action.description),
+            )
+            for action in actions
+        },
+    )
+
+    return create_model(
+        "Response",
+        response=(str, Field(..., description="The text response.")),
+        actions=(actions_model, Field(..., description="The actions to take.")),
+    )
+
+
+# TODO: make a model for the input data or just use Agent here
+def create_chain(
+    system_message: str, actions: list[Action]
+) -> Runnable[dict, BaseModel]:  # TODO: replace input with a model/TypedDict
     """Creates an LLM chain."""
 
     chat_model = ChatOpenAI(model=OPENAI_MODEL)
@@ -25,6 +50,10 @@ def create_chain(system_message: str, actions: list[Action]) -> Runnable:
         ]
     )
 
-    chat_model = chat_model.bind_tools([action.to_tool() for action in actions])
+    structured_output = create_structured_output(actions)
+
+    chat_model = chat_model.with_structured_output(
+        structured_output, method="json_schema", strict=True
+    )
 
     return prompt | chat_model
